f474fbf71aa3e34e490d9839c3239d42ec820ae8
ttruj <ttruj@tamu.edu>
2023-11-08 22:25:10 -0600

    Added Sample_Sort files

---
+++ Merge_Sort/cuda_merge_sort.cu
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+#include <caliper/cali.h>
+#include <caliper/cali-manager.h>
+#include <adiak.hpp>
+
+#include <cuda_runtime.h>
+#include <cuda.h>
+
---
+++ Merge_Sort/mpi_merge_sort.cpp
@@ -0,0 +1,8 @@
+#include "mpi.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+#include <caliper/cali.h>
+#include <caliper/cali-manager.h>
+#include <adiak.hpp>
